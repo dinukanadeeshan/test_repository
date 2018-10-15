d78eda12fb8662a54916d38c09a971caf6b31a1b
dinukanadeeshan <dinuka.n@eng.pdn.ac.lk>
2018-10-15 15:24:59 +0530

    Create add function

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
+void add(int a, int b);
+
 int main(){
 	printf("Hello World\n");
+	add(10, 20);
 	return 0;
+}
+
+void add(int a, int b){
+	int c = a + b;
+	printf("addition of %d and %d is %d\n", a, b, c);
 }
