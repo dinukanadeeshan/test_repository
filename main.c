6291b91183fa133d0f049aaf7b2e5d27eb2ea365
dinukanadeeshan <dinuka.n@eng.pdn.ac.lk>
2018-10-15 15:38:19 +0530

    Create sub function

--- main.c
+++ main.c
@@ -1,14 +1,21 @@
 #include<stdio.h>
 
 void add(int a, int b);
+void sub(int a, int b);
 
 int main(){
 	printf("Hello World\n");
 	add(10, 20);
+	sub(30, 20);
 	return 0;
 }
 
 void add(int a, int b){
 	int c = a + b;
 	printf("addition of %d and %d is %d\n", a, b, c);
+}
+
+void sub(int a, int b){
+	int c = a - b;
+	printf("subtraction of %d and %d is %d\n", a, b, c);
 }
